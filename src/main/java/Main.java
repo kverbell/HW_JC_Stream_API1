54a00f9c5ba226894100eb03ddabaa045e0edb3a
Anna Savrova <annsavrova@gmail.com>
2023-08-04 10:26:18 +0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> intNatList = new ArrayList<>();
+
+        for (int i : intList) {
+            if (i > 0 && i % 2 == 0) {
+                intNatList.add(i);
+            }
+        }
+        sortList(intNatList);
+        System.out.println(intNatList);
+    }
+
+    static void sortList(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int t = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, t);
+                }
+            }
+        }
+    }
+}
